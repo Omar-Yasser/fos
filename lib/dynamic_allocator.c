1d1fd600c2278fda4d9a9279cc64603e23fad894
Omar-Yasser <omaryasserhamed@gmail.com>
2022-10-29 18:23:31 +0200

    Refactor alloc_block_FF and alloc_block_BF

--- lib/dynamic_allocator.c
+++ lib/dynamic_allocator.c
@@ -120,39 +120,40 @@ void insert_sorted_allocList(struct MemBlock *blockToInsert)
     // TODO: [PROJECT MS1] [DYNAMIC ALLOCATOR] insert_sorted_allocList
     //  Write your code here, remove the panic and write your code
     // panic("insert_sorted_allocList() is not implemented yet...!!");
-
     insert_block(blockToInsert, &AllocMemBlocksList);
 }
 
 //=========================================
 // [4] ALLOCATE BLOCK BY FIRST FIT:
 //=========================================
+struct MemBlock *divide_block(struct MemBlock *blockToDivide, uint32 size)
+{
+    if (blockToDivide->size == size)
+    {
+        LIST_REMOVE(&FreeMemBlocksList, blockToDivide);
+        return blockToDivide;
+    }
+    struct MemBlock *newBlock = LIST_FIRST(&AvailableMemBlocksList);
+    LIST_REMOVE(&AvailableMemBlocksList, newBlock);
+
+    newBlock->size = size;
+    newBlock->sva = blockToDivide->sva;
+    blockToDivide->size -= size;
+    blockToDivide->sva += size;
+
+    return newBlock;
+}
 struct MemBlock *alloc_block_FF(uint32 size)
 {
     // TODO: [PROJECT MS1] [DYNAMIC ALLOCATOR] alloc_block_FF
     //  Write your code here, remove the panic and write your code
     // panic("alloc_block_FF() is not implemented yet...!!");
-    struct MemBlock *blk = FreeMemBlocksList.lh_first;
-    // LIST_FOREACH(blk,&(FreeMemBlocksList));
-    while (blk->prev_next_info.le_next != NULL)
+    struct MemBlock *blk;
+    LIST_FOREACH(blk, &FreeMemBlocksList)
     {
-        if (blk->size == size)
-        {
-            LIST_REMOVE(&(FreeMemBlocksList), blk);
-            return lastAllocBlk = blk;
-        }
-        else if (blk->size > size)
-        {
-            struct MemBlock *newBlock = LIST_FIRST(&AvailableMemBlocksList);
-            LIST_REMOVE(&AvailableMemBlocksList, newBlock);
-
-            newBlock->size = size;
-            newBlock->sva = blk->sva;
-            blk->size -= size;
-            blk->sva += size;
-            return lastAllocBlk = newBlock;
-        }
-        blk = blk->prev_next_info.le_next;
+        if (blk->size < size)
+            continue;
+        return lastAllocBlk = divide_block(blk, size);
     }
     return NULL;
 }
@@ -166,58 +167,31 @@ struct MemBlock *alloc_block_BF(uint32 size)
     //  Write your code here, remove the panic and write your code
     // panic("alloc_block_BF() is not implemented yet...!!");
     struct MemBlock *blk, *BF;
-    uint32 maxi = __UINT32_MAX__;
+    uint32 minSize = __UINT32_MAX__;
     LIST_FOREACH(blk, &FreeMemBlocksList)
     {
-        if (blk->size >= size && blk->size < maxi)
+        if (blk->size >= size && blk->size < minSize)
         {
             BF = blk;
-            maxi = blk->size;
+            minSize = blk->size;
         }
     }
-    if (maxi == __UINT32_MAX__)
+    if (minSize == __UINT32_MAX__)
         return NULL;
-    if (BF->size == size)
-    {
-        LIST_REMOVE(&FreeMemBlocksList, BF);
-        return lastAllocBlk = BF;
-    }
-    struct MemBlock *newBlock = LIST_FIRST(&AvailableMemBlocksList);
-    LIST_REMOVE(&AvailableMemBlocksList, newBlock);
-    newBlock->size = size;
-    newBlock->sva = BF->sva;
-    BF->size -= size;
-    BF->sva += size;
-    return lastAllocBlk = newBlock;
+    return lastAllocBlk = divide_block(BF, size);
 }
 
 //=========================================
 // [7] ALLOCATE BLOCK BY NEXT FIT:
 //=========================================
-
 struct MemBlock *find_block_NF(uint32 size, uint32 sva)
 {
     struct MemBlock *blk;
     LIST_FOREACH(blk, &FreeMemBlocksList)
     {
-        if (blk->sva < sva)
+        if (blk->sva < sva || blk->size < size)
             continue;
-        if (blk->size == size)
-        {
-            LIST_REMOVE(&FreeMemBlocksList, blk);
-            return lastAllocBlk = blk;
-        }
-        if (blk->size > size)
-        {
-            struct MemBlock *newBlock = LIST_FIRST(&AvailableMemBlocksList);
-            LIST_REMOVE(&AvailableMemBlocksList, newBlock);
-
-            newBlock->size = size;
-            newBlock->sva = blk->sva;
-            blk->size -= size;
-            blk->sva += size;
-            return lastAllocBlk = newBlock;
-        }
+        return lastAllocBlk = divide_block(blk, size);
     }
     return NULL;
 }
@@ -226,7 +200,6 @@ struct MemBlock *alloc_block_NF(uint32 size)
     // TODO: [PROJECT MS1 - BONUS] [DYNAMIC ALLOCATOR] alloc_block_NF
     //  Write your code here, remove the panic and write your code
     // panic("alloc_block_NF() is not implemented yet...!!");
-
     struct MemBlock *block_NF = find_block_NF(size, (lastAllocBlk == NULL ? 0 : lastAllocBlk->sva));
     if (block_NF == NULL)
         block_NF = find_block_NF(size, 0);
@@ -236,9 +209,8 @@ struct MemBlock *alloc_block_NF(uint32 size)
 //===================================================
 // [8] INSERT BLOCK (SORTED WITH MERGE) IN FREE LIST:
 //===================================================
-
 void merge_freeList(struct MemBlock *l, struct MemBlock *r)
-{   
+{
     if (l == NULL || r == NULL)
         return;
     if (l->sva + l->size == r->sva)
@@ -258,7 +230,6 @@ void insert_sorted_with_merge_freeList(struct MemBlock *blockToInsert)
     // TODO: [PROJECT MS1] [DYNAMIC ALLOCATOR] insert_sorted_with_merge_freeList
     //  Write your code here, remove the panic and write your code
     // panic("insert_sorted_with_merge_freeList() is not implemented yet...!!");
-
     insert_block(blockToInsert, &FreeMemBlocksList);
     merge_freeList(blockToInsert, blockToInsert->prev_next_info.le_next);
     merge_freeList(blockToInsert->prev_next_info.le_prev, blockToInsert);
