9b51d44538630ef38781a8c3cfa4d5cb8b443dc8
Omar-Yasser <omaryasserhamed@gmail.com>
2022-11-24 15:49:16 +0200

    Implement initialize_dyn_block_system

--- kern/mem/kheap.c
+++ kern/mem/kheap.c
@@ -6,92 +6,100 @@
 
 //==================================================================//
 //==================================================================//
-//NOTE: All kernel heap allocations are multiples of PAGE_SIZE (4KB)//
+// NOTE: All kernel heap allocations are multiples of PAGE_SIZE (4KB)//
 //==================================================================//
 //==================================================================//
 
 void initialize_dyn_block_system()
 {
-	//TODO: [PROJECT MS2] [KERNEL HEAP] initialize_dyn_block_system
-	// your code is here, remove the panic and write your code
-	kpanic_into_prompt("initialize_dyn_block_system() is not implemented yet...!!");
+    // TODO: [PROJECT MS2] [KERNEL HEAP] initialize_dyn_block_system
+    //  your code is here, remove the panic and write your code
+    //  kpanic_into_prompt("initialize_dyn_block_system() is not implemented yet...!!");
 
-	//[1] Initialize two lists (AllocMemBlocksList & FreeMemBlocksList) [Hint: use LIST_INIT()]
+    //[1] Initialize two lists (AllocMemBlocksList & FreeMemBlocksList) [Hint: use LIST_INIT()]
+
+    LIST_INIT(&AllocMemBlocksList);
+    LIST_INIT(&FreeMemBlocksList);
 #if STATIC_MEMBLOCK_ALLOC
-	//DO NOTHING
+    // DO NOTHING
 #else
-	/*[2] Dynamically allocate the array of MemBlockNodes
-	 * 	remember to:
-	 * 		1. set MAX_MEM_BLOCK_CNT with the chosen size of the array
-	 * 		2. allocation should be aligned on PAGE boundary
-	 * 	HINT: can use alloc_chunk(...) function
-	 */
+    /*[2] Dynamically allocate the array of MemBlockNodes
+     * 	remember to:
+     * 		1. set MAX_MEM_BLOCK_CNT with the chosen size of the array
+     * 		2. allocation should be aligned on PAGE boundary
+     * 	HINT: can use alloc_chunk(...) function
+     */
+    MAX_MEM_BLOCK_CNT = (uint32)(KERNEL_HEAP_MAX - KERNEL_HEAP_START) / PAGE_SIZE;
+    MemBlockNodes = (struct MemBlock *)KERNEL_HEAP_START;
+    allocate_chunk(ptr_page_directory, KERNEL_HEAP_START, (sizeof(struct MemBlock)) * MAX_MEM_BLOCK_CNT, PERM_WRITEABLE | PERM_PRESENT);
 #endif
-	//[3] Initialize AvailableMemBlocksList by filling it with the MemBlockNodes
-	//[4] Insert a new MemBlock with the remaining heap size into the FreeMemBlocksList
+    //[3] Initialize AvailableMemBlocksList by filling it with the MemBlockNodes
+    initialize_MemBlocksList(MAX_MEM_BLOCK_CNT);
+    //[4] Insert a new MemBlock with the remaining heap size into the FreeMemBlocksList
+    struct MemBlock *newBlock = LIST_FIRST(&AvailableMemBlocksList);
+    LIST_REMOVE(&AvailableMemBlocksList, newBlock);
+
+    newBlock->size = ((KERNEL_HEAP_MAX - KERNEL_HEAP_START) - ROUNDUP(MAX_MEM_BLOCK_CNT * sizeof(struct MemBlock), PAGE_SIZE));
+    newBlock->sva = (KERNEL_HEAP_START + ROUNDUP(MAX_MEM_BLOCK_CNT * sizeof(struct MemBlock), PAGE_SIZE));
+
+    LIST_INSERT_HEAD(&(FreeMemBlocksList), newBlock);
 }
 
-void* kmalloc(unsigned int size)
+void *kmalloc(unsigned int size)
 {
-	//TODO: [PROJECT MS2] [KERNEL HEAP] kmalloc
-	// your code is here, remove the panic and write your code
-	kpanic_into_prompt("kmalloc() is not implemented yet...!!");
+    // TODO: [PROJECT MS2] [KERNEL HEAP] kmalloc
+    //  your code is here, remove the panic and write your code
+    kpanic_into_prompt("kmalloc() is not implemented yet...!!");
 
-	//NOTE: All kernel heap allocations are multiples of PAGE_SIZE (4KB)
-	//refer to the project presentation and documentation for details
-	// use "isKHeapPlacementStrategyFIRSTFIT() ..." functions to check the current strategy
+    // NOTE: All kernel heap allocations are multiples of PAGE_SIZE (4KB)
+    // refer to the project presentation and documentation for details
+    //  use "isKHeapPlacementStrategyFIRSTFIT() ..." functions to check the current strategy
 
-	//change this "return" according to your answer
+    // change this "return" according to your answer
 }
 
-void kfree(void* virtual_address)
+void kfree(void *virtual_address)
 {
-	//TODO: [PROJECT MS2] [KERNEL HEAP] kfree
-	// Write your code here, remove the panic and write your code
-	panic("kfree() is not implemented yet...!!");
-
+    // TODO: [PROJECT MS2] [KERNEL HEAP] kfree
+    //  Write your code here, remove the panic and write your code
+    panic("kfree() is not implemented yet...!!");
 }
 
 unsigned int kheap_virtual_address(unsigned int physical_address)
 {
-	//TODO: [PROJECT MS2] [KERNEL HEAP] kheap_virtual_address
-	// Write your code here, remove the panic and write your code
-	panic("kheap_virtual_address() is not implemented yet...!!");
+    // TODO: [PROJECT MS2] [KERNEL HEAP] kheap_virtual_address
+    //  Write your code here, remove the panic and write your code
+    panic("kheap_virtual_address() is not implemented yet...!!");
 
-	//return the virtual address corresponding to given physical_address
-	//refer to the project presentation and documentation for details
-	//EFFICIENT IMPLEMENTATION ~O(1) IS REQUIRED ==================
+    // return the virtual address corresponding to given physical_address
+    // refer to the project presentation and documentation for details
+    // EFFICIENT IMPLEMENTATION ~O(1) IS REQUIRED ==================
 }
 
 unsigned int kheap_physical_address(unsigned int virtual_address)
 {
-	//TODO: [PROJECT MS2] [KERNEL HEAP] kheap_physical_address
-	// Write your code here, remove the panic and write your code
-	panic("kheap_physical_address() is not implemented yet...!!");
+    // TODO: [PROJECT MS2] [KERNEL HEAP] kheap_physical_address
+    //  Write your code here, remove the panic and write your code
+    panic("kheap_physical_address() is not implemented yet...!!");
 
-	//return the physical address corresponding to given virtual_address
-	//refer to the project presentation and documentation for details
+    // return the physical address corresponding to given virtual_address
+    // refer to the project presentation and documentation for details
 }
-
 
 void kfreeall()
 {
-	panic("Not implemented!");
-
+    panic("Not implemented!");
 }
 
 void kshrink(uint32 newSize)
 {
-	panic("Not implemented!");
+    panic("Not implemented!");
 }
 
 void kexpand(uint32 newSize)
 {
-	panic("Not implemented!");
+    panic("Not implemented!");
 }
-
-
-
 
 //=================================================================================//
 //============================== BONUS FUNCTION ===================================//
@@ -108,7 +116,7 @@ void kexpand(uint32 newSize)
 
 void *krealloc(void *virtual_address, uint32 new_size)
 {
-	//TODO: [PROJECT MS2 - BONUS] [KERNEL HEAP] krealloc
-	// Write your code here, remove the panic and write your code
-	panic("krealloc() is not implemented yet...!!");
+    // TODO: [PROJECT MS2 - BONUS] [KERNEL HEAP] krealloc
+    //  Write your code here, remove the panic and write your code
+    panic("krealloc() is not implemented yet...!!");
 }
